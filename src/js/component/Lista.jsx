16bc80306b7de74bb43ef02527aa8ceacd273950
LuciaRuy <luciaruy1@gmail.com>
2024-07-10 19:11:19 +0000

    list.fetch

--- src/js/component/Lista.jsx
+++ src/js/component/Lista.jsx
@@ -5,7 +5,7 @@ const List = () => {
     const [lista, setLista] = useState([]);
     const [usuario, setUsuario] = useState([])
 
-    
+
     function crearUsuario() {
         fetch('https://playground.4geeks.com/todo/users/LuciaRuy', {
             method: 'POST',
@@ -13,63 +13,114 @@ const List = () => {
                 "Content-Type": "application/json"
             }
         })
-        .then((response) => response.json())
-        .then((data) => console.log(data))
-        .catch((error) => console.log(error));
-    }
-    
-    function obtenerTareas() {
-        fetch('https://playground.4geeks.com/todo/users/LuciaRuy', {
-            method: 'GET',
-            headers: {
-                "Content-Type": "application/json"
+            .then((respose) => {
+                if (respose.status === 201) {
+                    obtenerTareas()
+                }
+                return respose.json()
+            })
+                    .then((data) => console.log(data))
+                    .catch((error) => console.log(error));
+           
             }
-        })
-        .then((response) => response.json())
-        .then((data) => setLista(data.todos))
-        .catch((error) => console.log(error))
+       
+            
+    // OBTIENE TODAS LAS TAREAS.        
+    function obtenerTareas() {
+                    fetch('https://playground.4geeks.com/todo/users/LuciaRuy', {
+                        method: 'GET',
+                        headers: {
+                            "Content-Type": "application/json"
+                        }
+                    })
+                        .then((respose) => {
+                            if (respose.status === 404) {
+                                crearUsuario()
+                            }
+                            console.log(respose)
+
+                            return respose.json()
+
+                        })
+
+                        .then((data) => setLista(data.todos))
+                        .catch((error) => console.log(error))
+                }
+
+
+    //ESTA FUNCION CREA UNA TAREA
+    function crearTareas() {
+                    fetch('https://playground.4geeks.com/todo/todos/LuciaRuy', {
+                        method: 'POST',
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify({
+                            "label": tarea,
+                            "is_done": false,
+                        })
+                    })
+                         .then((respose) => {
+                            if (respose.status === 201) {
+                                obtenerTareas()
+                            }
+                            console.log(respose)
+
+                            return respose.json()
+
+                        })
+                        .then((data) => console.log(data))
+                        .catch((error) => console.log(error));
+
+                }
+
+    function eliminarDesdeApi(id) {
+                    fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
+                        method: 'DELETE',
+                            headers: {
+                            "Content-type": "application / json"
+
+                        }
+                    })
+
+            .then((response) => {
+                console.log(response)
+                if (response.status === 204) {
+                    obtenerTareas()
+                } 
+            })
+                 
+            .then((data) => console.log(data))
+            .catch((error) => console.log(error));
+
     }
-
-
 
 
     function handleTarea(event) {
         setTarea(event.target.value);
     }
 
-    function agregarTarea() {
-        if (tarea.trim()) {
-            setLista([...lista, tarea]);
-            setTarea("");
-        }
-    }
 
-    function eliminarTarea(index) {
-        const nuevasTareas = [...lista];
-        nuevasTareas.splice(index, 1);
-        setLista(nuevasTareas);
-    }
-
-    useEffect(() =>{
+    useEffect(() => {
         crearUsuario()
         obtenerTareas()
-    },[])
+    }, [])
 
     return (
         <div className="card">
             <div className="card-body">
-                <input type="text"className="form-control" value={tarea} onChange={handleTarea}/>
-                <button className="btn btn-primary mt-2" onClick={agregarTarea}>Agregar</button>
+                <input type="text" className="form-control" value={tarea} onChange={handleTarea} />
+                <button className="btn btn-primary mt-2" onClick={crearTareas}>Agregar</button>
                 <ul className="list-group mt-3">
                     {lista.length > 0 ? (
-                        lista.map((tarea, index) => (
+                        lista.map((item, index) => (
                             <li key={index}
                                 className="list-group-item d-flex justify-content-between align-items-center"
                                 onMouseEnter={(e) => e.currentTarget.querySelector('button').style.display = 'inline'}
                             >
-                                {tarea.label}
-                                <button className="btn btn-danger btn-sm" onClick={() => eliminarTarea(index)}> Eliminar</button>
-                                </li>
+                                {item.label}
+                                <button className="btn btn-danger btn-sm" onClick={() => eliminarDesdeApi(item.id)}> Eliminar</button>
+                            </li>
                         ))
                     ) : null}
                 </ul>
